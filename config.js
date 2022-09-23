3e8436a2bf3ea52dae9507f6742ebd7ba3814b57
Poppy50 <113082812+Poppy50@users.noreply.github.com>
2022-09-24 04:37:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.xteam = 'apivproject'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.ownername = 'YahyaGanzz'
-global.botname = 'AzzBot•MD'
-global.packname = '© YahyaGanzz'
+global.owner = ['6281938591951']
+global.premium = ['6281938591951']
+global.ownername = 'Poppy_534'
+global.botname = 'Poppy-Bot'
+global.packname = '© poppy'
 global.gc = 'https://github.com/AzzBott679'
 global.linkyt = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
 global.linkgc = 'https://chat.whatsapp.com/CkguID8hSdSFAaBrKNHLSr'
